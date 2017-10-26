caf7b055b9ba52a5cb97e4f105598efd9a285735
Abinash <abinashkate@gmail.com>
2017-10-26 20:20:19 +0530

    Add simple change

--- JavaSample/src/com/Avi/progs/Array3.java
+++ JavaSample/src/com/Avi/progs/Array3.java
@@ -5,13 +5,16 @@ public class Array3 {
 		  
 		//declaring and initializing 2D array  
 		int arr[][]={{1,2,3},{4,5,6},{7,8,9}};  
+		System.out.println("Array matrix");
 		  
 		//printing 2D array  e
 		for(int i=0;i<3;i++){  
 		 for(int j=0;j<3;j++){  
 		   System.out.print(arr[i][j]+" ");  
 		 }  
+		 
 		 System.out.println();  
+		 
 		}  
 		  
 	}  
